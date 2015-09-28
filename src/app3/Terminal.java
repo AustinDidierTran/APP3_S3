6d16a47654164095ecc5a71409e30a02203ef6f9
Austin-Didier Tran <tran.austin.didier@gmail.com>
2015-09-28 12:19:13 -0400

    First commit

---
+++ src/app3/Terminal.java
@@ -0,0 +1,29 @@
+package app3;
+
+/** @author Ahmed Khoumsi */
+
+/** Cette classe identifie les terminaux reconnus et retournes par
+ *  l'analyseur lexical
+ */
+public class Terminal {
+
+
+// Constantes et attributs
+//  ....
+	String chaine;
+
+/** Un ou deux constructeurs (ou plus, si vous voulez)
+  *   pour l'initalisation d'attributs 
+ */	
+  public Terminal( ) {   // arguments possibles
+     
+	  //
+	  
+  }
+  public Terminal(String s) {
+	  
+	  
+	  
+  }
+
+}
